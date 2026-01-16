cda39dcd1a6f7a445e297010c3fc42003a6e53a5
yuvan66 <yuvanudt@gmail.com>
2026-01-16 13:13:59 +0530

    Modified initial commit

---
+++ LED-Blinking.c
@@ -0,0 +1,16 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+  DDRB = 0b00100000;
+  while (1)
+  {
+    PORTB = 0b00000000;
+    _delay_ms(500);
+    PORTB = 0b00100000;
+    _delay_ms(500);
+  }
+}
